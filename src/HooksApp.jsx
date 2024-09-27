e68158555a4eed611faca386b4cd30e1259e036c
Daniela3012 <danielapaola301299@gmail.com>
2024-09-27 08:24:52 -0500

    Login basico con React

---
+++ src/HooksApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FormularioComponent } from './components/FormularioComponent'
+
+export const HooksApp = () => {
+  return (
+    <>
+        <h1>Formulario de inscripción</h1>
+        <hr />
+        <FormularioComponent />
+    </>
+  )
+}
---
+++ src/hooks/useForm.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useForm = (initialForm = {}) => {
+    const [formState, setFormState]=useState({
+        userName: 'Daniela Pomalaya',
+        email: 'holis@gmail.com',
+        password: '123456789'
+      });
+  
+      const {userName, email, password} = formState;
+      const onInputChange = ({target}) => {
+        const {name, value} = target;
+        setFormState({
+          ...formState, 
+          [name]: value
+        });
+      }
+  return {
+    formState,
+    onInputChange
+  }
+}
